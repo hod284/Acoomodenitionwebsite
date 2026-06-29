b87e77c84a104e8c228eda41609ffe906401189b
hod284 <61046278+hod284@users.noreply.github.com>
2026-06-29 22:06:19 +0900

    1212
    12

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="sn-footer">
-      <p>STAYNEST 데모 · 결제 연동: MTouch(케이원피에스) 온라인인증결제 API</p>
-    </footer>
-  );
-}
--- src/utils/format.js
+++
@@ -1,3 +0,0 @@
-export const won = (n) => n.toLocaleString("ko-KR") + "원";
-
-export const genTrackId = () => "STN" + Date.now() + Math.floor(Math.random() * 1000);
